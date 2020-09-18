a258cc1b1e9be4bc8424f05d29d0b4a8d38d4442
muthu2404 <demigodpubg@gmail.com>
2020-09-18 09:41:39 +0530

    Second

---
+++ src/AverageCalculation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AverageCalculation {
+	
+	int student1=0;
+	int student2=0;
+	int student3=0;
+	double avg;
+	
+	for(inti=1;i<=3;i++)
+		
+	{
+		
+	}
+	
+	
+	
+
+}
---
+++ src/String.java
@@ -0,0 +1,12 @@
+
+
+public class String {
+	public static void main(String[] args) {
+	String s = "Hello";
+	String s1 ="Hello";
+		
+		
+	System.out.println(s.equals(s1));
+	
+	}
+}
